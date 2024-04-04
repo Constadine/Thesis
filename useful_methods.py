17b42275cee66e0f32e8fb0c615599f0d6cb791b
Konstantinos Papagiannopoulos <conpap95@live.com>
2024-04-04 21:26:07 +0200

    Create function to discard files. Load only necessary files

--- useful_methods.py
+++ useful_methods.py
@@ -1,6 +1,8 @@
+import os
+import shutil
 import pandas as pd
 
-def load_and_clean_climate_data(file_path, config):
+def load_climate_data(file_path, config, oldest_data_to_keep=2015):
     # Read the first few lines of the file to determine the number of rows to skip
     num_skip_rows = 0
     with open(file_path, 'r') as file:
@@ -11,7 +13,6 @@ def load_and_clean_climate_data(file_path, config):
     
     # Reading the file based on the dynamically determined number of rows to skip
     df = pd.read_csv(file_path, skiprows=num_skip_rows - 1, delimiter=config['delimiter'], engine='python')
-    
 
     # Check if 'Datum' and 'Tid (UTC)' columns exist
     if 'Datum' in df.columns and 'Tid (UTC)' in df.columns:
@@ -23,15 +24,54 @@ def load_and_clean_climate_data(file_path, config):
         # Rename the column 'Datum Tid (UTC)' to 'Date'
         df.rename(columns={'Datum Tid (UTC)': 'Date'}, inplace=True)
 
+
     
-    # Make Date first column again
+    
+ # Filter the DataFrame to include only rows where the year in the 'Date' column is greater than or equal to oldest_data_to_keep
+    df['Date'] = pd.to_datetime(df['Date'])  # Ensure 'Date' column is in datetime format
+    df = df[df['Date'].dt.year >= oldest_data_to_keep]
+
+    # Make 'Date' first column again
     df = df[['Date'] + [col for col in df.columns if col != 'Date']]
+
+    # Slice and rename columns after filtering
     df = df.iloc[:, :2]
     df.columns = config['column_names']
-    date_column = config['date_column']
-    df[date_column] = pd.to_datetime(df[date_column]) 
+
+    # Make Date first column again
+    df = df[['Date'] + [col for col in df.columns if col != 'Date']]
     
+
     return df
+
+def load_bird_data(file_path):
+    bird_data = pd.read_csv(file_path)
+    bird_data.drop(columns='Unnamed: 0', inplace=True)
+
+    return bird_data
+  
+def discard_files_with_old_data(folder_path, config, oldest_data_to_keep=2015):    
+    # Directory to move discarded files
+    discarded_dir = os.path.join(folder_path, 'discarded_old_stations')
+
+    # Create the directory if it doesn't exist
+    if not os.path.exists(discarded_dir):
+        os.makedirs(discarded_dir)
+
+    for filename in os.listdir(folder_path):
+        if filename.endswith(".csv"):
+            file_path = os.path.join(folder_path, filename)
+            df = load_climate_data(file_path, config, 1800)
+            date_column = config['date_column']
+            max_year = df[date_column].dt.year.max()
+            if max_year < oldest_data_to_keep:
+                file_name = os.path.basename(file_path)
+                new_path = os.path.join(discarded_dir, file_name)
+                print(f'Moving {file_name} to {discarded_dir} since it contains data until {max_year}.')
+                shutil.move(file_path, new_path)
+            else:
+                print(f'Max year {df[date_column].dt.year.max()} - OK')
+
 
 def find_observation_coords(file_path):
     
@@ -70,3 +110,15 @@ def find_observation_coords(file_path):
             row_index += 1
             
     return latitude, longitude
+
+from climate_configs import configs
+
+data_folder = 'data/SMHI/'
+for main_folder in os.listdir(data_folder):
+    for folder in os.listdir(data_folder+main_folder):
+        variable_folder = os.path.join(data_folder+main_folder+'/'+folder)
+        print(variable_folder)
+        config = configs[folder]
+    
+        discard_files_with_old_data(variable_folder, config)
+    
